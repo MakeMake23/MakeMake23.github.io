ffbb8c02d7a17c800ec6a4915076ea22bed05f93
MakeMake23 <andy.ledesma.garcia@gmail.com>
2025-07-17 18:41:57 +0400

    feat: make favicon be my face. Use CustomCursor in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import favicon16 from "../assets/images/favicon-16x16.png";
+import favicon32 from "../assets/images/favicon-32x32.png";
+import favicon48 from "../assets/images/favicon-48x48.png";
+import favicon180 from "../assets/images/favicon-180x180.png";
+import favicon192 from "../assets/images/favicon-192x192.png";
+import favicon512 from "../assets/images/favicon-512x512.png";
+import ethCursor from "../assets/images/eth-cursor.png";
+import ethCursorClick from "../assets/images/eth-cursor-click.gif";
+import CustomCursor from "@/components/CustomCursor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +23,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Andy Ledesma García | CTO & Computer Scientist",
-  description: "CTO & Computer Scientist & Blockchain Developer & Back-end Developer",
-  keywords: "Andy Ledesma García, CTO, Computer Scientist, Blockchain Developer, Back-end Developer",
+  description:
+    "CTO & Computer Scientist & Blockchain Developer & Back-end Developer",
+  keywords:
+    "Andy Ledesma García, CTO, Computer Scientist, Blockchain Developer, Back-end Developer",
 };
 
 export default function RootLayout({
@@ -25,14 +36,38 @@ export default function RootLayout({
   children: React.ReactNode;
   params?: { lang?: string };
 }>) {
-  // Default to 'en' if no language parameter is available
-  const lang = params?.lang || 'en';
-  
+  const lang = params?.lang || "en";
+
   return (
     <html lang={lang}>
+
+      <head>
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16.src} />
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon32.src} />
+        <link rel="icon" type="image/png" sizes="48x48" href={favicon48.src} />
+        <link rel="apple-touch-icon" sizes="180x180" href={favicon180.src} />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href={favicon192.src}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href={favicon512.src}
+        />
+        <style>{`
+          body, a, button, [role="button"], input[type="submit"], input[type="button"], input[type="reset"], .clickable {
+            cursor: none !important; /* Hide the default cursor on all elements */
+          }
+        `}</style>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <CustomCursor defaultCursor={ethCursor.src} clickCursor={ethCursorClick.src} />
         {children}
       </body>
     </html>
