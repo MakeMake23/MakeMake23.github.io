28b400df797f1ac3044b832dfb495fec3e78f914
MakeMake23 <andy.ledesma.garcia@gmail.com>
2025-09-19 08:26:37 +0400

    build: make this project a Cloudflare worker

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/cv",
-        destination: "/en/cv",
-      },
-    ];
-  },
   // Enable proper Turbopack configuration
   experimental: {
     // Turbopack is enabled by default in dev command
@@ -16,3 +8,6 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
